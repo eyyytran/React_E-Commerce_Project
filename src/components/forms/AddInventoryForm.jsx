bab177c56b1de04740fd45f620a167b61b8ca48b
eyyytran <ltatran94@gmail.com>
2022-08-28 00:11:57 -0400

    add data to collections db

--- src/components/forms/AddInventoryForm.jsx
+++ src/components/forms/AddInventoryForm.jsx
@@ -1,5 +1,8 @@
 import { productTypes, productDescription } from '../utils'
 import { useState } from 'react'
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../../firebaseConfig'
+
 const AddInventoryForm = () => {
     const [name, setName] = useState('')
     const [type, setType] = useState(null)
@@ -8,7 +11,35 @@ const AddInventoryForm = () => {
     const [actives, setActives] = useState('')
     const [ingredients, setIngredients] = useState('')
     const [usage, setUsage] = useState('')
+    const [imageURL1, setImageURL1] = useState('')
+    const [imageURL2, setImageURL2] = useState('')
+    const [imageURL3, setImageURL3] = useState('')
     const [qty, setQty] = useState(0)
+
+    const handleClick = async e => {
+        try {
+            e.preventDefault()
+
+            const result = await addDoc(collection(db, 'products'), {
+                name,
+                type,
+                subtitle,
+                description,
+                actives,
+                ingredients,
+                usage,
+                imageURL1,
+                imageURL2,
+                imageURL3,
+                qty,
+            })
+
+            console.log(result.id)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <form>
             <input
@@ -67,12 +98,35 @@ const AddInventoryForm = () => {
             />
 
             <input
+                type='text'
+                name='imageUrl'
+                placeholder='Primary Image'
+                value={imageURL1}
+                onChange={e => setImageURL1(e.target.value)}
+            />
+            <input
+                type='text'
+                name='imageUrl'
+                placeholder='Secondary Image'
+                value={imageURL2}
+                onChange={e => setImageURL2(e.target.value)}
+            />
+            <input
+                type='text'
+                name='imageUrl'
+                placeholder='Tertiary Image'
+                value={imageURL3}
+                onChange={e => setImageURL3(e.target.value)}
+            />
+
+            <input
                 type='number'
                 name='quanity'
                 placeholder='QTY'
                 value={qty}
                 onChange={e => setQty(e.target.value)}
             />
+            <button onClick={handleClick}>Add Product</button>
         </form>
     )
 }
