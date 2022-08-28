42ef1e61be2908f7c21864fac41ff494d2655a3b
eyyytran <ltatran94@gmail.com>
2022-08-28 14:16:15 -0400

    react query and getDocs on admin inventory functioning

--- src/components/AdminInventory.jsx
+++ src/components/AdminInventory.jsx
@@ -1,5 +1,37 @@
+import { collection, getDocs } from 'firebase/firestore'
+import { useQueryClient, useQuery } from 'react-query'
+import { db } from '../firebaseConfig'
+
 const AdminInventory = () => {
-    return <div>AdminInventory</div>
+    const fetchInventory = async () => {
+        const products = []
+        const docs = await getDocs(collection(db, 'products'))
+        docs.forEach(doc => {
+            products.push(doc.data())
+        })
+        return products
+    }
+
+    const queryClient = useQueryClient()
+
+    const { data, error, isError, isLoading } = useQuery(['products'], () =>
+        fetchInventory()
+    )
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    if (isError) {
+        return <div>Error: {error.message}</div>
+    }
+
+    return (
+        <div className='admin_inventory-container'>
+            {data?.map(product => {
+                return <div>{product.name}</div>
+            })}
+        </div>
+    )
 }
 
 export default AdminInventory
