96d7693a206d2edc920dce37eb8843f2dc9ffd1d
eyyytran <ltatran94@gmail.com>
2022-08-28 17:45:17 -0400

    mild styling on admin page

--- src/components/AdminInventoryItem.jsx
+++ src/components/AdminInventoryItem.jsx
@@ -1,7 +1,10 @@
-import '../styles/InventoryItem.css'
+import { useState } from 'react'
 import EditInventoryForm from './forms/EditInventoryForm'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import '../styles/InventoryItem.css'
 
 const AdminInventoryItem = ({ product }) => {
+    const [active, setActive] = useState(false)
     return (
         <div className='admin_inventory-item_container'>
             <div className='admin_inventory-item'>
@@ -9,8 +12,17 @@ const AdminInventoryItem = ({ product }) => {
                 <div className='inventory-item-name'>{product?.name}</div>
                 <div className='inventory-item-price'>{product?.price}</div>
                 <div className='inventory-item-qty'>{product?.qty}</div>
+                <button onClick={() => setActive(!active)}>
+                    <ExpandMoreIcon />
+                </button>
             </div>
-            <div className='admin_edit-module'>
+            <div
+                className={
+                    active === true
+                        ? 'admin_edit-module '
+                        : 'admin_edit-module hidden'
+                }
+            >
                 <EditInventoryForm product={product} key={product?.id} />
             </div>
         </div>
