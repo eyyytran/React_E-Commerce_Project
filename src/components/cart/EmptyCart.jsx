f09c85a60f306dcb95dd965d2efd4a5c76ea63a0
eyyytran <ltatran94@gmail.com>
2022-08-29 17:43:36 -0400

    commit before changing caching

---
+++ src/components/cart/EmptyCart.jsx
@@ -0,0 +1,9 @@
+const EmptyCart = () => {
+    return (
+        <div className='empty-cart-container'>
+            <p>Your shopping bag is empty</p>
+        </div>
+    )
+}
+
+export default EmptyCart
--- src/layouts/Cart.jsx
+++ src/layouts/Cart.jsx
@@ -1,18 +1,21 @@
 import { useSelector } from 'react-redux'
 import CartCalculations from '../components/cart/CartCalculations'
 import CartItem from '../components/cart/CartItem'
+import EmptyCart from '../components/cart/EmptyCart'
 
 const Cart = () => {
     const cartContents = useSelector(state => state.cart.cart)
     return (
         <div className='cart'>
-            <h1>Your Cart</h1>
+            <h1>Your Bag</h1>
             <div className='cart-item-container'>
-                {cartContents?.length === 0
-                    ? 'Your cart is empty'
-                    : cartContents?.map(product => {
-                          return <CartItem product={product} key={product.id} />
-                      })}
+                {cartContents?.length === 0 ? (
+                    <EmptyCart />
+                ) : (
+                    cartContents?.map(product => {
+                        return <CartItem product={product} key={product.id} />
+                    })
+                )}
             </div>
             <div className='cart-calculation-container'>
                 <CartCalculations />
