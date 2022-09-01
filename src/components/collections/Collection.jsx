7de2614d0599c718aeb023f572d20041a3b7e5dd
eyyytran <ltatran94@gmail.com>
2022-09-01 09:52:20 -0400

    mui grid for collections

--- src/components/collections/Collection.jsx
+++ src/components/collections/Collection.jsx
@@ -1,6 +1,9 @@
 import { useParams } from 'react-router-dom'
 import { useQueryClient, useQuery } from 'react-query'
 import { fetchCollectionByType, fetchInventory } from '../utils/dbRequests'
+import { Grid } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
 import ProductCard from './ProductCard'
 
 const Collection = () => {
@@ -27,11 +30,22 @@ const Collection = () => {
     }
 
     return (
-        <div>
+        <div className='collection-container'>
             <h1>{params.collection.toUpperCase()}</h1>
-            {data?.map(product => {
-                return <ProductCard product={product} key={product.id} />
-            })}
+            <div className='collection'>
+                <Grid container spacing={1}>
+                    {data?.map(product => {
+                        return (
+                            <Grid item xs={6}>
+                                <ProductCard
+                                    product={product}
+                                    key={product.id}
+                                />
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </div>
         </div>
     )
 }
--- src/layouts/Collections.jsx
+++ src/layouts/Collections.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom'
 const Collections = () => {
     return (
         <div className='collections-container'>
-            <h1>Collection</h1>
             <Outlet />
         </div>
     )
